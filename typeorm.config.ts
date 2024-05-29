b6087dea591601b7be7b95ed6a7da7fdd1b9ba4e
Carlos <carlossantistebang@gmail.com>
2024-05-29 00:33:51 -0600

    Segundo Commit

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -2,11 +2,11 @@ import { DataSource } from "typeorm";
 
 export const dataSource = new DataSource({
     type:'postgres',
-    host: 'localhost', 
-    port: 5432, 
-    username: 'url', 
-    password: 'url.2024',
-    database: 'blog', 
+    host: 'monorail.proxy.rlwy.net', 
+    port: 20955, 
+    username: 'postgres', 
+    password: 'IfJQGdHJDmdcQLoVYjRSzJYTobQAMctj',
+    database: 'railway', 
     synchronize: false,
     entities: ['src/**/*.entity.ts'],
     migrations: ['./src/migrations/*.ts'],
